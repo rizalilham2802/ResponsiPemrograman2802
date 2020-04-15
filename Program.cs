78170f1cb2c5e886abb8fcf7fa8a3360f48f8668
mukhamad rizal <mukhamad.ilham@students.amikom.ac.id>
2020-04-15 11:46:41 +0700

    ResponsiPemrograman2802

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2802
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan Karyawan1 = new karyawan ("1.","190302123","Paijo",3000000);
+            karyawan Karyawan2 = new karyawan("2.", "190302124", "Jono",2000000);
+            Console.WriteLine("No\tNIK\t Nama\t  gaji Bulanan\t");
+            Console.WriteLine("----------------------------------");
+            Karyawan1.GajiMurni();
+            Karyawan2.GajiMurni();
+            Console.WriteLine();
+            Console.WriteLine("\nAsyiiiiik kenaikan gaji 10%!!\n");
+            Console.WriteLine("No\tNIK\t Nama\t  gaji Bulanan\t");
+            Console.WriteLine("----------------------------------");
+            Karyawan1.KenaikanGaji();
+            Karyawan2.KenaikanGaji();
+            Console.ReadKey();
+
+        
+        }
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2802
+{
+    class karyawan
+    {
+        public string No { get; set; }
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public karyawan(string no, string nik, string nama, int gajibulanan)
+        {
+            No = no;
+            Nik = nik;
+            Nama = nama;
+            if (gajibulanan < 0)
+            {
+                GajiBulanan = 0;
+            }
+            else
+            {
+                GajiBulanan = gajibulanan;
+            }
+        }
+        public void GajiMurni()
+        {
+            Console.WriteLine(" {0} {1}\t {2}\t {3}\t", No, Nik, Nama, GajiBulanan);
+        }
+        public void KenaikanGaji()
+        {
+            Console.WriteLine(" {0} {1}\t {2}\t {3}\t", No, Nik, Nama, (GajiBulanan * 110/100));
+        }
+    }
+}
